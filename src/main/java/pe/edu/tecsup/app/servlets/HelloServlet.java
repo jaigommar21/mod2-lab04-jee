9cd854477f409a0f98b83e351255421458a3cb4e
Jaime <jgomezm@tecsup.edu.pe>
2021-08-03 21:15:02 -0500

    Update

--- src/main/java/pe/edu/tecsup/app/servlets/HelloServlet.java
+++ src/main/java/pe/edu/tecsup/app/servlets/HelloServlet.java
@@ -31,9 +31,10 @@ public class HelloServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 
 		response.getWriter().
+				append("<html>").
 				append("<h2>Hola Mundo</h2>").
-				append("<p>Este es mi primer Servlet</p>");
-	
+				append("<p>Este es mi primer Servlet</p>").
+				append("</html>");
 	}
 
 	/**
